6b1e1b524e6e60b30b143a6295cd2bb5d9b92b29
PhilipHsu609 <mpyhacct118113@gmail.com>
2025-01-12 17:06:47 +0800

    Fist config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,10 +26,39 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+if not vim.g.vscode then
+  vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+  vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+  vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+  vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+else
+  -- VSCode keymaps
+  local code = require 'vscode'
+
+  -- Implementation
+  vim.keymap.set('n', 'gi', function()
+    code.action 'editor.action.goToImplementation'
+  end)
+  vim.keymap.set('n', 'gI', function()
+    code.action 'editor.action.peekImplementation'
+  end)
+
+  -- Definition
+  vim.keymap.set('n', 'gd', function()
+    code.action 'editor.action.revealDefinition'
+  end)
+  vim.keymap.set('n', 'gD', function()
+    code.action 'editor.action.peekDefinition'
+  end)
+
+  -- Reference
+  vim.keymap.set('n', 'gr', function()
+    code.action 'editor.action.goToReferences'
+  end)
+  vim.keymap.set('n', 'gR', function()
+    code.action 'editor.action.referenceSearch.trigger'
+  end)
+end
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
