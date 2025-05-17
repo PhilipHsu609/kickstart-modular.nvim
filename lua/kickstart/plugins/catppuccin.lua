10ffb1acaf4cc95259fa2ae4676c21d3ff9c7087
PhilipHsu609 <mpyhacct118113@gmail.com>
2025-05-17 16:48:54 +0800

    Add catppuccin

---
+++ lua/kickstart/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'catppuccin/nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      vim.cmd.colorscheme 'catppuccin-mocha'
+    end,
+  },
+}
+-- vim: ts=2 sts=2 sw=2 et
