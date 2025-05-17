339099a72c20ed6a0ddcdc884ae167ee3108cc5b
PhilipHsu609 <mpyhacct118113@gmail.com>
2025-05-17 16:49:16 +0800

    With VSCode

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -9,6 +9,8 @@
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
+local vscode = vim.g.vscode ~= nil
+
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
@@ -23,25 +25,20 @@ require('lazy').setup({
   -- modular approach: using `require 'path/name'` will
   -- include a plugin definition from file lua/path/name.lua
 
-  require 'kickstart/plugins/gitsigns',
-
-  require 'kickstart/plugins/which-key',
-
-  require 'kickstart/plugins/telescope',
-
-  require 'kickstart/plugins/lspconfig',
-
-  require 'kickstart/plugins/conform',
-
-  require 'kickstart/plugins/blink-cmp',
-
-  require 'kickstart/plugins/tokyonight',
-
-  require 'kickstart/plugins/todo-comments',
-
-  require 'kickstart/plugins/mini',
-
-  require 'kickstart/plugins/treesitter',
+  -- Plugins that should only load when not in VSCode
+  not vscode and {
+    require 'kickstart/plugins/gitsigns',
+    require 'kickstart/plugins/which-key',
+    require 'kickstart/plugins/telescope',
+    require 'kickstart/plugins/lspconfig',
+    require 'kickstart/plugins/mini',
+    require 'kickstart/plugins/todo-comments',
+    require 'kickstart/plugins/conform',
+    require 'kickstart/plugins/blink-cmp',
+    require 'kickstart/plugins/treesitter',
+  } or nil,
+  
+  require 'kickstart/plugins/catppuccin',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
