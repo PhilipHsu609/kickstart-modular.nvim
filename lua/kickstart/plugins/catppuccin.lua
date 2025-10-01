1a9277a62b7cb35f6801997f755ca130881b6937
PhilipHsu609 <mpyhacct118113@gmail.com>
2025-10-01 22:46:35 +0800

    Update theme

--- lua/kickstart/plugins/catppuccin.lua
+++ lua/kickstart/plugins/catppuccin.lua
@@ -3,7 +3,7 @@ return {
     'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      vim.cmd.colorscheme 'catppuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin-frappe'
     end,
   },
 }
