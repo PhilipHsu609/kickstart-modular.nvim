a0e1c97ce6dcf01f6fbde716fecb1ef9ee695744
PhilipHsu609 <mpyhacct118113@gmail.com>
2025-12-30 20:03:30 +0800

    Refactor keymaps

---
+++ lua/keymaps/vscode.lua
@@ -0,0 +1,53 @@
+-- [[ VSCode-Neovim Integration Keymaps ]]
+-- Keymaps specific to VSCode-Neovim integration
+--  See: https://github.com/vscode-neovim/vscode-neovim
+
+-- VSCode keymaps
+local code = require 'vscode'
+
+-- Navigations
+vim.keymap.set('n', '<C-h>', function()
+  code.action 'workbench.action.previousEditor'
+end, { desc = 'Move focus to the left window' })
+
+vim.keymap.set('n', '<C-l>', function()
+  code.action 'workbench.action.nextEditor'
+end, { desc = 'Move focus to the right window' })
+
+vim.keymap.set('n', '<leader>wt', function()
+  code.call 'workbench.view.testing.focus'
+end, { desc = 'Move focus to the testing view' })
+
+vim.keymap.set('n', '<leader>we', function()
+  code.call 'workbench.view.explorer'
+end, { desc = 'Move focus to the explorer view' })
+
+vim.keymap.set('n', '<leader>q', function()
+  code.action 'workbench.action.closeActiveEditor'
+end, { desc = 'Close active editor' })
+
+-- Implementation
+vim.keymap.set('n', 'gi', function()
+  code.action 'editor.action.goToImplementation'
+end)
+vim.keymap.set('n', 'gI', function()
+  code.action 'editor.action.peekImplementation'
+end)
+
+-- Definition
+vim.keymap.set('n', 'gd', function()
+  code.action 'editor.action.revealDefinition'
+end)
+vim.keymap.set('n', 'gD', function()
+  code.action 'editor.action.peekDefinition'
+end)
+
+-- Reference
+vim.keymap.set('n', 'gr', function()
+  code.action 'editor.action.goToReferences'
+end)
+vim.keymap.set('n', 'gR', function()
+  code.action 'editor.action.referenceSearch.trigger'
+end)
+
+-- vim: ts=2 sts=2 sw=2 et
